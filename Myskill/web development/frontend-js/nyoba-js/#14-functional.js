c04244c58c875faabba51973bad6cc1d9c72be82
arena-belajar <septaranja@gmail.com>
2025-09-09 23:06:17 +0700

    pindah folder

---
+++ Myskill/web development/frontend-js/nyoba-js/#14-functional.js
@@ -0,0 +1,12 @@
+```
+
+                EcmaScript 2
+    Functional Programming & Destructive Assignment
+
+
+    1. Functional Programming
+        paradigma pemrograman yang didominasi oleh fungsi-fungsi
+
+```
+
+// contoh bukan functional
