4ffc1914cc11001fb03eaa647c5e864ae1a32456
arena-belajar <septaranja@gmail.com>
2025-11-03 12:16:48 +0700

    vue

---
+++ Myskill/web development/vue/main.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <app-header></app-header>
+            <div class="container"
+                <ul>
+                    <li>Honda</li>
+                    <li>Toyota</li>
+                    <li>Ford</li>
+                </ul>
+        <app-footer></app-footer>
+    </div>
+    </div>
+</template>
+
+
+
+
+
+<script>
+import { Component } from 'react';
+
+
+export default {
+    Components: {
+        compBrand
+    },
+
+    data() {
+        return {
+            cars: [
+                {model: 'Jazz', brand: 'Honda'},
+                {model: 'Yaria', brand: 'Toyota'},
+                {model: 'Airew', brand: 'Mulling'},
+            ]
+        }
+    },
+}
+
+provide() {
+    
+}
+
+</script>
