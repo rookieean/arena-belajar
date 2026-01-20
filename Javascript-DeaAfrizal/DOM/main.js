a18d4cff1f95fba5da9561c63b11e61886926bf8
rookiee.an <septaranja@gmail.com>
2026-01-20 21:54:38 +0700

    save js dea

--- Javascript-DeaAfrizal/DOM/main.js
+++ Javascript-DeaAfrizal/DOM/main.js
@@ -1,9 +1,16 @@
 const btn1 = document.getElementById('btn1')
-const btn2 = document.getElementById('btn2')
+const btn = document.getElementById('button')
+const body = document.body
 
-console.log(btn2)
 
 // buttonny mengarah ke sini
 function gantiWarna() {
     btn1.style.background = 'hotpink'
+}
+
+function clickButton() {
+    btn1.style.background = 'aqua'
+    const newText = document.createElement('p')
+    newText.textContent = 'Hallo'
+    body.append(newText)
 }
